c9769c66fb084343f468aeb81498663574583554
ONUR TASHAN <onurtashan@gmail.com>
2017-06-23 11:11:08 +0300

    "MONGODB'nin default olarak localhost'a bakmasını kapatıyorum."

--- src/main/java/com/tashan/excelparser/ExcelparserApplication.java
+++ src/main/java/com/tashan/excelparser/ExcelparserApplication.java
@@ -1,9 +1,12 @@
 package com.tashan.excelparser;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class}) //MongoDB'nin localhost'a bakmasını kapatıyoruz.
 public class ExcelparserApplication {
 
 	public static void main(String[] args) {
